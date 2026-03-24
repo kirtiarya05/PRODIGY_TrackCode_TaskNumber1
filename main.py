c147e77db247580c100876ca2091fb6058c48586
Kirti Arya <251186791+kirtiarya05@users.noreply.github.com>
2026-03-24 11:50:23 +0530

    Remove unnecessary blank line in main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Ridge
 from sklearn.preprocessing import StandardScaler
